8bfc41c30037713900d78b2e599efe89945b6ef4
SviatoslavSlichnyi <Mygithub1510>
2021-05-16 12:13:00 +0300

    init

---
+++ src/test/java/university/lab/arrival/ArrivalApplicationTests.java
@@ -0,0 +1,13 @@
+package university.lab.arrival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
